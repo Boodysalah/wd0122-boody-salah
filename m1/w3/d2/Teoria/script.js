96dffbda60fc3d18ddcd9b9ead2eb7d92d958509
Boodysalah <ojalacafevalencia@gmail.com>
2022-04-12 19:23:08 +0200

    smanettare e provare

--- m1/w3/d2/Teoria/script.js
+++ m1/w3/d2/Teoria/script.js
@@ -1,7 +1,18 @@
-function sfondoRosso(){
+function cambiaSfondo(e){
+   
+
+   /* let blocchi = document.getElementsByClassName ("blocco")
 
 
+    blocchi[e].style.backgroundColor = "red"*/
 
+    let colore = e.getAttribute("data-colore")
+    console.log(colore);
 
-    
+    e.style.backgroundColor = colore;
+
 }
+
+
+
+
